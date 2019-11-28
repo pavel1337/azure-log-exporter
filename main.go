7a3302bc51c70465064ed78d11d845e1e5183b59
Pavel <pavel.voronov@paygee.com>
2019-11-28 14:32:11 +0100

    lookup ipv6 as well, bc microsoft cant do ipv6

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"io/ioutil"
 	"errors"
+	"net"
+	"net/http"
 	"flag"
 	"os"
 	"log"
@@ -27,6 +29,34 @@ type Config struct {
     TenantID 				string 			`json:"msgraph_tenantID"`
     ApplicationID 			string 			`json:"msgraph_appID"`
     SecretKey 				string 			`json:"msgraph_secretKey"`
+    AppNameInGraylog 		string 			`json:"app_name_in_graylog"`
+}
+
+type IPLookUpData struct {
+	Status      string `json:"status"`
+	Description string `json:"description"`
+	Data        struct {
+		Geo struct {
+			Host          string      `json:"host"`
+			IP            string      `json:"ip"`
+			Rdns          string      `json:"rdns"`
+			Asn           int         `json:"asn"`
+			Isp           string      `json:"isp"`
+			CountryName   string      `json:"country_name"`
+			CountryCode   string      `json:"country_code"`
+			RegionName    string      `json:"region_name"`
+			RegionCode    string      `json:"region_code"`
+			City          string      `json:"city"`
+			PostalCode    string      `json:"postal_code"`
+			ContinentName string      `json:"continent_name"`
+			ContinentCode string      `json:"continent_code"`
+			Latitude      float64     `json:"latitude"`
+			Longitude     float64     `json:"longitude"`
+			MetroCode     interface{} `json:"metro_code"`
+			Timezone      string      `json:"timezone"`
+			Datetime      string      `json:"datetime"`
+		} `json:"geo"`
+	} `json:"data"`
 }
 
 type GELFInstance struct {
@@ -47,35 +77,6 @@ type GELFInstance struct {
 	LocationState 				string	`json:"_location_state"`
 	LocationCountry 			string	`json:"_location_country"`
 	GeoData						string  `json:"_geodata"`
-}
-
-func NewGelfLog(s msgraph.Signin) ([]byte, error) {
-	gi := GELFInstance{}
-	gi.Timestamp = s.CreatedDateTime.Unix()
-	hn, err := os.Hostname()
-	if err != nil {
-		fmt.Errorf("Hostname problem: %v", err)
-		os.Exit(1)
-	}
-	gi.AppName = "AzureADLogs"
-	gi.Host = hn
-	gi.DeviceDetail = s.DeviceDetail.OperatingSystem + " " + s.DeviceDetail.Browser
-	gi.Location = s.Location.City + " " + s.Location.State + " " + s.Location.CountryOrRegion
-	gi.LocationCity = s.Location.City
-	gi.LocationState = s.Location.State
-	gi.LocationCountry = s.Location.CountryOrRegion
-	gi.ShortMessage = s.UserDisplayName + " from " + gi.Location  + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
-	gi.ID = s.ID
-	gi.UserDisplayName = s.UserDisplayName
-	gi.UserPrincipalName = s.UserPrincipalName
-	gi.AppDisplayName = s.AppDisplayName
-	gi.IpAddress = s.IpAddress
-	gi.ClientAppUsed = s.ClientAppUsed
-	gi.ResourceDisplayName = s.ResourceDisplayName
-	gi.GeoData = strconv.FormatFloat(s.Location.GeoCoordinates.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(s.Location.GeoCoordinates.Longitude, 'f', 6, 64)
-
-    message, err := json.Marshal(gi)
-    return message, err
 }
 
 func main() {
@@ -122,7 +123,7 @@ func main() {
 				if err != nil {
 					log.Println(err)
 				}
-				msg, _ := NewGelfLog(signin)
+				msg, _ := NewGelfLog(signin, c.AppNameInGraylog)
         		g.Log(string(msg))
 			}
 		}
@@ -130,6 +131,86 @@ func main() {
 		time.Sleep(1 * time.Minute)
 	}
 
+}
+
+func Ipv6LookUp(ip string) IPLookUpData {
+
+    defer func() {
+        if r := recover(); r != nil {}
+    }()
+
+	c := http.Client{
+		Timeout: time.Second * 10, // Maximum of 10 secs
+	}
+
+	url := "https://tools.keycdn.com/geo.json?host=" + ip
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Panic("client error:", err)
+	}
+
+	res, getErr := c.Do(req)
+	if getErr != nil {
+		log.Panic("request error:", getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Panic("response error:", readErr)
+	}
+
+	data := IPLookUpData{}
+	jsonErr := json.Unmarshal(body, &data)
+	if jsonErr != nil {
+		log.Panic("json parse error:", jsonErr)
+	}
+
+	return data
+}
+
+func NewGelfLog(s msgraph.Signin, AppNameInGraylog string) ([]byte, error) {
+	gi := GELFInstance{}
+	gi.Timestamp = s.CreatedDateTime.Unix()
+	hn, err := os.Hostname()
+	if err != nil {
+		fmt.Errorf("Hostname problem: %v", err)
+		os.Exit(1)
+	}
+	gi.AppName = AppNameInGraylog
+	gi.Host = hn
+	gi.DeviceDetail = s.DeviceDetail.OperatingSystem + " " + s.DeviceDetail.Browser
+	gi.ID = s.ID
+	gi.UserDisplayName = s.UserDisplayName
+	gi.UserPrincipalName = s.UserPrincipalName
+	gi.AppDisplayName = s.AppDisplayName
+	if IsIpv4Net(s.IpAddress) {
+		gi.IpAddress = s.IpAddress
+		gi.Location = s.Location.City + " " + s.Location.State + " " + s.Location.CountryOrRegion
+		gi.LocationCity = s.Location.City
+		gi.LocationState = s.Location.State
+		gi.LocationCountry = s.Location.CountryOrRegion
+		gi.GeoData = strconv.FormatFloat(s.Location.GeoCoordinates.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(s.Location.GeoCoordinates.Longitude, 'f', 6, 64)
+		gi.ShortMessage = s.UserDisplayName + " from " + gi.Location  + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
+	} else {
+		d := Ipv6LookUp(s.IpAddress)
+		gi.IpAddress = s.IpAddress
+		gi.Location = d.Data.Geo.City + " " + d.Data.Geo.RegionName + " " + d.Data.Geo.CountryCode
+		gi.LocationCity = d.Data.Geo.City
+		gi.LocationState = d.Data.Geo.RegionName
+		gi.LocationCountry = d.Data.Geo.CountryCode
+		gi.GeoData = strconv.FormatFloat(d.Data.Geo.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(d.Data.Geo.Longitude, 'f', 6, 64)
+		gi.ShortMessage = s.UserDisplayName + " from " + gi.Location  + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
+	}
+	gi.ClientAppUsed = s.ClientAppUsed
+	gi.ResourceDisplayName = s.ResourceDisplayName
+
+    message, err := json.Marshal(gi)
+    return message, err
+}
+
+func IsIpv4Net(host string) bool {
+   return net.ParseIP(host) != nil
 }
 
 func parseFlags() (*string) {
@@ -146,7 +227,7 @@ func parseConfig(p string) (Config, error) {
     if err != nil {
 		flag.Usage()
         if errors.As(err, &pathError) {
-            return c, errors.New("Please create '/etc/azure-log-exporter/config.yml'\nExample:\n   redis_address: '1.1.1.1:6369'\n   redis_expiration: 1 (in hours)\n   graylog_host: '1.1.1.1'\n   graylog_port: 12201\n   msgraph_tenantID: '<TenantID>' \n   msgraph_appID: '<ApplicationID>' \n   msgraph_secretKey: '<secretkey>' \n")
+            return c, errors.New("Please create '/etc/azure-log-exporter/config.yml'\nExample:\n   redis_address: '1.1.1.1:6369'\n   redis_expiration: 1 (in hours)\n   graylog_host: '1.1.1.1'\n   graylog_port: 12201\n   msgraph_tenantID: '<TenantID>' \n   msgraph_appID: '<ApplicationID>' \n   msgraph_secretKey: '<secretkey>' \n   app_name_in_graylog: 'name_you_want_to_see_in_application_name_field' \n")
         }
         return c, err
     }
