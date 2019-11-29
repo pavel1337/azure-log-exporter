8ff59e04e23f21c36759e76d4f963d3fbf27e2f7
Pavel <pavel.voronov@paygee.com>
2019-11-29 08:38:40 +0100

    whops, proper ip version check has just landed

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"time"
 	"encoding/json"
-	"strconv"
-	"io/ioutil"
 	"errors"
+	"flag"
+	"fmt"
+	"github.com/ghodss/yaml"
+	"github.com/go-redis/redis"
+	"github.com/pavel1337/go-msgraph"
+	"github.com/robertkowalski/graylog-golang"
+	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
-	"flag"
 	"os"
-	"log"
-	"github.com/pavel1337/go-msgraph"
-    "github.com/ghodss/yaml"
-	"github.com/go-redis/redis"
-    "github.com/robertkowalski/graylog-golang"
-
+	"strconv"
+	"time"
 )
 
 var pathError *os.PathError
 
 type Config struct {
-    RedisAddress 			string 			`json:"redis_address"`
-    RedisExpiration 		time.Duration 	`json:"redis_expiration"`
-    GraylogHost 			string 			`json:"graylog_host"`
-    GraylogPort 			int 			`json:"graylog_port"`
-    TenantID 				string 			`json:"msgraph_tenantID"`
-    ApplicationID 			string 			`json:"msgraph_appID"`
-    SecretKey 				string 			`json:"msgraph_secretKey"`
-    AppNameInGraylog 		string 			`json:"app_name_in_graylog"`
+	RedisAddress     string        `json:"redis_address"`
+	RedisExpiration  time.Duration `json:"redis_expiration"`
+	GraylogHost      string        `json:"graylog_host"`
+	GraylogPort      int           `json:"graylog_port"`
+	TenantID         string        `json:"msgraph_tenantID"`
+	ApplicationID    string        `json:"msgraph_appID"`
+	SecretKey        string        `json:"msgraph_secretKey"`
+	AppNameInGraylog string        `json:"app_name_in_graylog"`
 }
 
 type IPLookUpData struct {
@@ -60,39 +59,39 @@ type IPLookUpData struct {
 }
 
 type GELFInstance struct {
-	Timestamp         			int64	`json:"timestamp"`
-	ID                      	string	`json:"_signin_id"`
-    Host 						string  `json:"host"`
-    AppName 					string  `json:"application_name"`
-    ShortMessage 				string  `json:"short_message"`
-	UserPrincipalName       	string	`json:"_user_Principal_Name"`
-	UserDisplayName         	string	`json:"_user_Display_Name"`
-	AppDisplayName          	string	`json:"_app_Display_Name"`
-	IpAddress               	string	`json:"_Ip_Address"`
-	ClientAppUsed				string	`json:"_client_App_Used"`
-	ResourceDisplayName			string	`json:"_resourse_Display_Name"`
-	DeviceDetail				string	`json:"_device_detail"`
-	Location 					string	`json:"_location"`
-	LocationCity 				string	`json:"_location_city"`
-	LocationState 				string	`json:"_location_state"`
-	LocationCountry 			string	`json:"_location_country"`
-	GeoData						string  `json:"_geodata"`
+	Timestamp           int64  `json:"timestamp"`
+	ID                  string `json:"_signin_id"`
+	Host                string `json:"host"`
+	AppName             string `json:"application_name"`
+	ShortMessage        string `json:"short_message"`
+	UserPrincipalName   string `json:"_user_Principal_Name"`
+	UserDisplayName     string `json:"_user_Display_Name"`
+	AppDisplayName      string `json:"_app_Display_Name"`
+	IpAddress           string `json:"_Ip_Address"`
+	ClientAppUsed       string `json:"_client_App_Used"`
+	ResourceDisplayName string `json:"_resourse_Display_Name"`
+	DeviceDetail        string `json:"_device_detail"`
+	Location            string `json:"_location"`
+	LocationCity        string `json:"_location_city"`
+	LocationState       string `json:"_location_state"`
+	LocationCountry     string `json:"_location_country"`
+	GeoData             string `json:"_geodata"`
 }
 
 func main() {
 	path := parseFlags()
-    
-    c, err := parseConfig(*path)
-    if err != nil {
-        log.Fatalf("ERROR: %s", err)
-        return
-    }
+
+	c, err := parseConfig(*path)
+	if err != nil {
+		log.Fatalf("ERROR: %s", err)
+		return
+	}
 
 	graphClient, err := msgraph.NewGraphClient(c.TenantID, c.ApplicationID, c.SecretKey)
 	if err != nil {
-	    fmt.Println(err)
+		fmt.Println(err)
 	}
-	
+
 	rc := redis.NewClient(&redis.Options{
 		Addr:     c.RedisAddress,
 		Password: "", // no password set
@@ -100,20 +99,19 @@ func main() {
 	})
 	dur := c.RedisExpiration * time.Hour
 
-
 	t := time.Now().UTC().Add(time.Minute * -10)
 	filter := "createdDateTime ge " + t.Format("2006-01-02T15:04:05Z")
 
 	var gc gelf.Config
-    gc.GraylogPort = c.GraylogPort
-    gc.GraylogHostname = c.GraylogHost
-    g := gelf.New(gc)   
+	gc.GraylogPort = c.GraylogPort
+	gc.GraylogHostname = c.GraylogHost
+	g := gelf.New(gc)
 
 	for {
 		t = time.Now().UTC().Add(time.Minute * -10)
 		list, err := graphClient.ListSignInsWithFilter(filter)
 		if err != nil {
-		    fmt.Println(err)
+			fmt.Println(err)
 		}
 		for i := range list {
 			signin := list[i]
@@ -124,7 +122,7 @@ func main() {
 					log.Println(err)
 				}
 				msg, _ := NewGelfLog(signin, c.AppNameInGraylog)
-        		g.Log(string(msg))
+				g.Log(string(msg))
 			}
 		}
 		filter = "createdDateTime ge " + t.Format("2006-01-02T15:04:05Z")
@@ -135,9 +133,10 @@ func main() {
 
 func Ipv6LookUp(ip string) IPLookUpData {
 
-    defer func() {
-        if r := recover(); r != nil {}
-    }()
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
 
 	c := http.Client{
 		Timeout: time.Second * 10, // Maximum of 10 secs
@@ -191,7 +190,7 @@ func NewGelfLog(s msgraph.Signin, AppNameInGraylog string) ([]byte, error) {
 		gi.LocationState = s.Location.State
 		gi.LocationCountry = s.Location.CountryOrRegion
 		gi.GeoData = strconv.FormatFloat(s.Location.GeoCoordinates.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(s.Location.GeoCoordinates.Longitude, 'f', 6, 64)
-		gi.ShortMessage = s.UserDisplayName + " from " + gi.Location  + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
+		gi.ShortMessage = s.UserDisplayName + " from " + gi.Location + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
 	} else {
 		d := Ipv6LookUp(s.IpAddress)
 		gi.IpAddress = s.IpAddress
@@ -200,40 +199,43 @@ func NewGelfLog(s msgraph.Signin, AppNameInGraylog string) ([]byte, error) {
 		gi.LocationState = d.Data.Geo.RegionName
 		gi.LocationCountry = d.Data.Geo.CountryCode
 		gi.GeoData = strconv.FormatFloat(d.Data.Geo.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(d.Data.Geo.Longitude, 'f', 6, 64)
-		gi.ShortMessage = s.UserDisplayName + " from " + gi.Location  + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
+		gi.ShortMessage = s.UserDisplayName + " from " + gi.Location + " with " + gi.DeviceDetail + " via " + s.ResourceDisplayName
 	}
 	gi.ClientAppUsed = s.ClientAppUsed
 	gi.ResourceDisplayName = s.ResourceDisplayName
 
-    message, err := json.Marshal(gi)
-    return message, err
+	message, err := json.Marshal(gi)
+	return message, err
 }
 
 func IsIpv4Net(host string) bool {
-   return net.ParseIP(host) != nil
+	testInput := net.ParseIP(host)
+	if testInput.To4() == nil {
+		return false
+	}
+	return true
 }
 
-func parseFlags() (*string) {
-    configPathHelpInfo := " path to config file"
-    configPath := flag.String("c", "", configPathHelpInfo)
-    flag.Parse()
-    return configPath
+func parseFlags() *string {
+	configPathHelpInfo := " path to config file"
+	configPath := flag.String("c", "", configPathHelpInfo)
+	flag.Parse()
+	return configPath
 }
-
 
 func parseConfig(p string) (Config, error) {
-    var c Config
-    rawConfig, err := ioutil.ReadFile(p)
-    if err != nil {
+	var c Config
+	rawConfig, err := ioutil.ReadFile(p)
+	if err != nil {
 		flag.Usage()
-        if errors.As(err, &pathError) {
-            return c, errors.New("Please create '/etc/azure-log-exporter/config.yml'\nExample:\n   redis_address: '1.1.1.1:6369'\n   redis_expiration: 1 (in hours)\n   graylog_host: '1.1.1.1'\n   graylog_port: 12201\n   msgraph_tenantID: '<TenantID>' \n   msgraph_appID: '<ApplicationID>' \n   msgraph_secretKey: '<secretkey>' \n   app_name_in_graylog: 'name_you_want_to_see_in_application_name_field' \n")
-        }
-        return c, err
-    }
-    err = yaml.Unmarshal(rawConfig, &c)
-    if err != nil {
-        return c, err
-    }
-    return c, nil
+		if errors.As(err, &pathError) {
+			return c, errors.New("Please create '/etc/azure-log-exporter/config.yml'\nExample:\n   redis_address: '1.1.1.1:6369'\n   redis_expiration: 1 (in hours)\n   graylog_host: '1.1.1.1'\n   graylog_port: 12201\n   msgraph_tenantID: '<TenantID>' \n   msgraph_appID: '<ApplicationID>' \n   msgraph_secretKey: '<secretkey>' \n   app_name_in_graylog: 'name_you_want_to_see_in_application_name_field' \n")
+		}
+		return c, err
+	}
+	err = yaml.Unmarshal(rawConfig, &c)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
 }
